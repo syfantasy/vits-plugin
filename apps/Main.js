605d377008a98a202260d139b72ad53acdca5fbb
syfantasy <115546901+syfantasy@users.noreply.github.com>
2024-08-14 23:11:55 +0800

    Update Main.js

--- apps/Main.js
+++ apps/Main.js
@@ -1,27 +1,14 @@
-import plugin from '../../../lib/plugins/plugin.js'
-import Config from '../components/Config.js'
-import { TextToSpeech as BertVITSTextToSpeech } from '../components/Bert-VITS2.js';
-import { TextToSpeech as GPTSoVITSTextToSpeech } from '../components/GPT-SoVITS.js';
-import { TextToSpeech as FishAudioTTSTextToSpeech } from '../components/Fish-Audio.js';
-import { getRecord } from '../components/Record.js'
-
 export class vits_sync extends plugin {
   constructor() {
     super({
-      /** 功能名称 */
       name: 'VITS-同传',
-      /** 功能描述 */
-      dsc: '同声传译',
+      dsc: '同音传声',
       event: 'message',
-      /** 优先级，数字越小等级越高 */
       priority: 99999,
       rule: [
         {
-          /** 命令正则匹配 */
           reg: '',
-          /** 执行方法 */
           fnc: 'sync',
-          /** 关闭日志 */
           log: false
         }
       ]
@@ -32,30 +19,14 @@ export class vits_sync extends plugin {
     try {
       if (!e.group_id || !e.user_id) return false;
 
-      const { tts_sync_config: config, tts_config: { send_base64 } } = await Config.getConfig();
+      const { tts_sync_config: config } = await Config.getConfig();
       const c = config.find(item => item.user_id == e.user_id);
-      if (!c || !c.use_speaker) return false;
+      if (!c) return false;
 
       if (!c.enable_group.includes(String(e.group_id))) return false;
 
-      let url;
-
-      if (c.use_model_type == 'GPT-SoVITS') {
-        url = await GPTSoVITSTextToSpeech(c.use_speaker, e.msg, c);
-      } else if (c.use_model_type == 'Fish-Audio') {
-        url = await FishAudioTTSTextToSpeech(c.use_speaker, e.msg, c);
-      } else {
-        url = await BertVITSTextToSpeech(c.use_speaker, e.msg, c);
-      }
-
-      if (!url) return false;
-
-      if (send_base64) {
-        const base64 = await getRecord(url);
-        await e.reply(segment.record(`base64://${base64}`));
-      } else {
-        await e.reply(segment.record(url));
-      }
+      // 在这里可以添加你的自定义逻辑，比如记录日志或进行其他处理
+      logger.info(`用户 ${e.user_id} 在群 ${e.group_id} 发送了消息：${e.msg}`);
 
       return false;
     } catch (err) {
